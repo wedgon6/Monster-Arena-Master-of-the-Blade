9666025ef510780999cc1a5d7bc142d44788c2a5
wedgon6 <wedgon6@yandex.ru>
2024-08-15 22:06:20 +0300

    Tutorial localization

--- Assets/Game/Scripts/EntryPoint/Tutorial.cs
+++ Assets/Game/Scripts/EntryPoint/Tutorial.cs
@@ -1,6 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    
+    [SerializeField] private List<TutorialPhrase> _dialoguePhrases;
+    [SerializeField] private TMP_Text _lable;
+    [SerializeField] private Image _icon;
+    [SerializeField] private EnemySpawner _enemySpawner;
+
+    private bool _isContinueDialog = false;
+    private Coroutine _corontine;
+
+    public void ContinueDialog()
+    {
+        _isContinueDialog = true;
+    }
+
+    public void SpotDialog()
+    {
+        if (_corontine != null)
+            StopCoroutine(_corontine);
+
+        EndDialogue();
+    }
+
+    private void Start()
+    {
+        _enemySpawner.PutEnemyToPool();
+
+        if (_corontine != null)
+            StopCoroutine(_corontine);
+
+        _corontine = StartCoroutine(RunStudyDialog());
+    }
+
+    private IEnumerator RunStudyDialog()
+    {
+        for (int i = 0; i < _dialoguePhrases.Count; i++)
+        {
+            _isContinueDialog = false;
+            _dialoguePhrases[i].UpdateLocalization();
+            _lable.text = _dialoguePhrases[i].Phease.text;
+            _icon.sprite = _dialoguePhrases[i].Icon;
+
+            yield return new WaitUntil(() => _isContinueDialog);
+        }
+
+        EndDialogue();
+    }
+
+    private void EndDialogue()
+    {
+        _enemySpawner.RestSpawner(0);
+        gameObject.SetActive(false);
+    }
 }
---
+++ Assets/Game/Scripts/EntryPoint/TutorialPhrase.cs
@@ -0,0 +1,18 @@
+using Lean.Localization;
+using TMPro;
+using UnityEngine;
+
+public class TutorialPhrase : MonoBehaviour
+{
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private TMP_Text _phease;
+    [SerializeField] private LeanLocalizedTextMeshProUGUI _localized;
+
+    public TMP_Text Phease => _phease;
+    public Sprite Icon => _icon;
+
+    public void UpdateLocalization()
+    {
+        _localized.UpdateTranslation(LeanLocalization.GetTranslation(_localized.TranslationName));
+    }
+}
